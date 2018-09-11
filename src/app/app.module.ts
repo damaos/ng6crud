e054d96d7667d6fc73d16e8dbf427c5eef5d7cb6
Danilo Martinez <danilo_98osorno@hotmail.com>
2018-09-10 19:56:52 -0500

    Se crean los componentes del Index, Create y Edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateComponent } from './components/create/create.component';
+import { IndexComponent } from './components/index/index.component';
+import { EditComponent } from './components/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    IndexComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule
